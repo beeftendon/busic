7e5f3ac77f2ab19e8b7b3cf5ca5d256435fe8cd8
eugene <eug.song@gmail.com>
2016-02-11 23:04:37 -0800

    Got basic melody generation working w/ button.

--- busic_arduino/busic_arduino.ino
+++ busic_arduino/busic_arduino.ino
@@ -1,11 +1,28 @@
-#include "notes.h"
+//#include "notes.h"
+
+#define numNotes 8
+
+char noteMap[numNotes] = {'C', 'D', 'E', 'F', 'G', 'A', 'B', 'Q'};
+int freqMap[numNotes] = {262, 294, 330, 349, 392, 440, 494, 523};
+
+void generateTone(int freq, int duration, int pin) {
+  int period = 1000000/freq; // us
+  int duty = period/2;
+  long startTime = millis();
+  long currTime = millis();
+  while (millis() - startTime < duration) {
+    digitalWrite(pin, 1);
+    delayMicroseconds(duty); // Approximately 10% duty cycle @ 1KHz
+    digitalWrite(pin, 0);
+    delayMicroseconds(period - duty);
+    currTime = millis();
+  }
+}
 
 int pin1 = 1;
 int pin0 = 0;
-int tonePin = 11;
+int tonePin = 6;
 volatile int state = LOW;
-int heart_led = 9;
-bool heart_on = false;
 
 long lastDebounceTime = 0;
 long debounceDelay = 300;
@@ -15,6 +32,7 @@ int buttonState;
 long lastButtonPress = 0;
 
 char melody[512];
+int melodyFreq[512];
 int count = 0;
 int curr = 0;
 
@@ -22,8 +40,6 @@ void setup() {
     pinMode(pin1, INPUT);
     pinMode(pin0, INPUT);
     pinMode(tonePin, OUTPUT);
-    pinMode(heart_led, OUTPUT);
-
     attachInterrupt(digitalPinToInterrupt(pin1), buttonPressed, RISING);
     attachInterrupt(digitalPinToInterrupt(pin0), buttonPressed, RISING);
 }
@@ -32,15 +48,16 @@ void loop() {
   if (curr > count) {
     curr = 0;
   }
-  Serial.println(melody[curr]);
-  curr = curr + 1;
-
-  if (heart_on) {
-    digitalWrite(heart_led, LOW);
-  } else {
-    digitalWrite(heart_led, HIGH);
+  //Serial.println(melody[curr]);
+  
+  int noteCount = 0;
+  while (noteCount < count) {
+    generateTone(melodyFreq[noteCount], 250, tonePin);
+    Serial.println(melodyFreq[noteCount]);
+    noteCount++;
   }
-  heart_on = !heart_on;
+  
+  curr = curr + 1;
 
   delay(300);
 }
@@ -49,7 +66,8 @@ void buttonPressed() {
   if ((millis() - lastDebounceTime) > debounceDelay) {
         long pressed = millis();
         melody[count] = noteMap[(pressed - lastButtonPress)%numNotes];
-        generateTone(freqMap[(pressed-lastButtonPress)%numNotes])
+        melodyFreq[count] = freqMap[(pressed - lastButtonPress)%numNotes];
+        //generateTone(freqMap[(pressed-lastButtonPress)%numNotes], 1000, tonePin);
         lastButtonPress = pressed;
         count = count + 1;
         lastDebounceTime = millis();
