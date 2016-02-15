441a7e948f65c1b56b29835dce14143a4b8c8761
aimichelle <aimichelle@berkeley.edu>
2016-02-15 11:48:29 -0800

    added note generation

--- busic_arduino/busic_arduino.ino
+++ busic_arduino/busic_arduino.ino
@@ -1,37 +1,44 @@
 #include <Tone.h>
 #include <Wire.h>
-#include <TimerOne.h>
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
+#include <TimerOne.h>
 
 #define numNotes 8
 
-char noteMap[numNotes] = {'C', 'D', 'E', 'F', 'G', 'A', 'B', 'Q'};
 int freqMap[numNotes] = {262, 294, 330, 349, 392, 440, 494, 523};
-int beat = 100;
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
-int pin1 = 1;
-int pin0 = 0;
 int tonePin = 6;
-volatile int state = LOW;
 
-long lastDebounceTime = 0;
-long debounceDelay = 1000;
-int lastButton = LOW;
-int buttonState;
-
-long lastButtonPress = 0;
-
-char melody[512];
 int melodyFreq[512];
-int holds[512];
+
 int count = 0;
 int curr = 0;
 
+// How long to hold notes
+int dur = 75;
+
+long lastDebounceTime = 0;
+long debounceDelay = 500;
+
 double accX = 0;
-bool newNote = false;
+double accY = 0;
+
+void generateTone(int freq, int duration, int pin) {
+  int period = 1000000/freq; // us
+  int duty = period/2;
+
+  int cnt = 0; // millis and micros don't work so well in interrupts...
+  while (cnt < duration) {
+    digitalWrite(pin, 1);
+    delayMicroseconds(duty); // Approximately 10% duty cycle @ 1KHz
+    digitalWrite(pin, 0);
+    delayMicroseconds(period - duty);
+    cnt ++;
+    }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -46,27 +53,46 @@ void setup() {
 
   // Set up tones
   pinMode(tonePin, OUTPUT);
+
+  Timer1.initialize(300000);    //700000, with note duration 150 is slowest 
+  Timer1.pwm(9, 512);                // setup pwm on pin 9, 50% duty cycle
+  Timer1.attachInterrupt(playNote);  
+}
+
+void playNote() {
+  if (curr >= count) {
+    curr = 0;
+  } 
+  else if (count > 0) {
+    generateTone(melodyFreq[curr], dur, tonePin);
+    curr++;
+  }
 }
 
 
 void loop() {
-  if (curr > count) {
-    curr = 0;
-  }
-  if (newNote) {
-    tone(tonePin, freqMap[0], 500);
-    newNote = false;
-  }
-
+    
   mma.read();
   sensors_event_t event; 
   mma.getEvent(&event);
   accX = event.acceleration.x;
-  if (accX > 2.0 || accX < -2.0) {
-    newNote = true;
+
+  if ((accX < -3.0) && ((millis() - lastDebounceTime) > debounceDelay)) {
+    lastDebounceTime = millis();
+    melodyFreq[count] = freqMap[int(-accX*100)%numNotes];
+    count++;
   }
   
-  Serial.println(accX);
-
-  delayMicroseconds(100);
+  accY = event.acceleration.y;
+  
+  // The slowest timer interrupt period is 700000, with a note duration of 150.
+  if (round(7 - abs(accY)) > 3) {
+    Timer1.initialize((100000 * round(7 - abs(accY))));
+    dur = int(150 * (7 - abs(accY))/7);    
+  } else { // Fastest possible time
+    Timer1.initialize(300000);
+    dur = 75;    
+  }
+  
+  delayMicroseconds(1000);
 }
